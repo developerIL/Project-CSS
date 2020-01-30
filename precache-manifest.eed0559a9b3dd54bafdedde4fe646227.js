093e75d785bc74191f5d67eb98be9ad2fa0bb10e
developerIL <jewdeveloper@gmail.com>
2020-01-30 09:27:42 +0200

    Updates

---
+++ precache-manifest.eed0559a9b3dd54bafdedde4fe646227.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b29f482a9690a3aaedc6cadf523c428",
+    "url": "/projects/index.html"
+  },
+  {
+    "revision": "3d56ba5a61c191c105c6",
+    "url": "/projects/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "6f01b2e1e1f4d81726cd",
+    "url": "/projects/static/css/main.087cf801.chunk.css"
+  },
+  {
+    "revision": "3d56ba5a61c191c105c6",
+    "url": "/projects/static/js/2.1e49f808.chunk.js"
+  },
+  {
+    "revision": "0a4ffb280925d1e048d1e4d65051ee0e",
+    "url": "/projects/static/js/2.1e49f808.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f01b2e1e1f4d81726cd",
+    "url": "/projects/static/js/main.a52d8a2c.chunk.js"
+  },
+  {
+    "revision": "adcddbe050ce5a03d9c3",
+    "url": "/projects/static/js/runtime-main.45b7a9b5.js"
+  },
+  {
+    "revision": "2e07bfc12b13773fb3a4313f3f1e2f9b",
+    "url": "/projects/static/media/app1.2e07bfc1.PNG"
+  },
+  {
+    "revision": "dc7b75ef7bfa662d12b47e9b403d7e56",
+    "url": "/projects/static/media/app2.dc7b75ef.PNG"
+  },
+  {
+    "revision": "4acda19aa33611dae771820fdeb55537",
+    "url": "/projects/static/media/app3.4acda19a.png"
+  },
+  {
+    "revision": "c336bcd3b9d3a383f9782f2141f5a9e8",
+    "url": "/projects/static/media/app4.c336bcd3.jpg"
+  },
+  {
+    "revision": "aad4aad0501b22462d20ed563c3101c6",
+    "url": "/projects/static/media/app5.aad4aad0.png"
+  },
+  {
+    "revision": "66d3ba6cf9e2f6d07c0003bbc77b5deb",
+    "url": "/projects/static/media/app6.66d3ba6c.jpg"
+  },
+  {
+    "revision": "3e7625e972f3515ab7bfe1f3e37cc4c5",
+    "url": "/projects/static/media/slide01.3e7625e9.jpg"
+  },
+  {
+    "revision": "2c64fa01fe0e717d3ab7b17acbfe0f5d",
+    "url": "/projects/static/media/slide02.2c64fa01.jpg"
+  }
+]);
